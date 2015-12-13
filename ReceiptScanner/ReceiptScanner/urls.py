f5b7e4e30833daebf0b6df74d528e0050e3f16f0
Norbert Podsadowski <Norbz@NORBO-MAC.local>
2015-12-13 00:04:35 +0000

    Done static files

--- ReceiptScanner/ReceiptScanner/urls.py
+++ ReceiptScanner/ReceiptScanner/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import include, url
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^main/', include('main.urls')),
     url(r'^api/', include('api.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
