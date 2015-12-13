7f57ffb05be314c3cd86e374dd57d5f5b68ee114
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2015-12-13 05:35:07 +0000

    Implemented budget views

--- ReceiptScanner/api/models.py
+++ ReceiptScanner/api/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class BudgetPeriod(models.Model):
+  user = models.ForeignKey(User)
+  start_date = models.DateTimeField()
+  end_date = models.DateTimeField()
+  spending_limit = models.DecimalField(max_digits=20, decimal_places=10)
+
 class Store(models.Model):
   name = models.CharField(max_length=255)
   location = models.CharField(max_length=255)
@@ -21,8 +27,3 @@ class ReceiptItem(models.Model):
   price = models.DecimalField(max_digits=20, decimal_places=10)
   category = models.ManyToManyField(Category)
   receipt = models.ForeignKey(Receipt)
-
-class BudgetPeriod(models.Model):
-  start_date = models.DateTimeField()
-  end_date = models.DateTimeField()
-  spending_limit = models.DecimalField(max_digits=20, decimal_places=10)
--- ReceiptScanner/main/views.py
+++ ReceiptScanner/main/views.py
@@ -5,8 +5,10 @@ from django.utils.decorators import method_decorator
 from main.forms import RegisterForm
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
-from datetime import datetime, timedelta
-import api.models 
+import datetime
+from api.models import Receipt
+from api.models import BudgetPeriod
+
 
 
 
@@ -47,37 +49,46 @@ class RegisterView(TemplateView):
     return render(request, self.template_name, self.get_context_data())
 
 class BudgetView(TemplateView):
-  
-  @method_decorator(login_required)
-  def get(self, request):
+
+  def get_context_data(self, **kwargs):
+    context = super(BudgetView, self).get_context_data(**kwargs)
+
     #Get list of receipts
-    currentUser = request.user
+    currentUser = self.request.user
     today = datetime.date.today()
     currentMonth = today.month
     currentYear = today.year
-    receipts = 0
-
-
-    Receipts.objects.filter(user = currentUser, date__year = currentYear, date__month = currentMonth)
+    receipts = Receipt.objects.filter(user = currentUser, date__year = currentYear, date__month = currentMonth)
+    receiptsNumber = receipts.count()
 
     #Get budget spent and number of items
     spentBudget = 0
-    itemsNumber = 0
+    itemsList = []
+
     
     for receipt in receipts:
-      items = receipt.receiptItem_set.all()
-      itemsNumber += items.count()
+      items = receipt.receiptitem_set.all()
       for item in items:
         spentBudget += item.price
+        itemsList.append(item)
 
     #Get current monthly budget
-    currentBudget = BudgetPeriod.objects.filter(start_date__month = currentMonth)[0]
+    currentBudget = BudgetPeriod.objects.filter(user = currentUser, start_date__month = currentMonth)[0]
     spendingLimit = currentBudget.spending_limit
+    print spendingLimit
 
     #Get left budget
     leftBudget = spendingLimit - spentBudget
-
-
-    return render(request, "main/budget.html")
+    
+    context["receipts"] = receipts
+    context["spentBudget"] = spentBudget
+    context["spendingLimit"] = spendingLimit
+    context["leftBudget"] = leftBudget
+    context["items"] = itemsList
+    return context
+  
+  @method_decorator(login_required)
+  def get(self, request):
+    return render(request, "main/budget.html", self.get_context_data())
 
 
